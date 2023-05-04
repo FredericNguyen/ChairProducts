dd52100c3198c6e3dcf66809c7e5a2d4b85f4db3
FredericNguyen <fredericnguyen@yahoo.com>
2023-05-03 20:16:05 -0400

    started products page and added instruction for index.js form

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 user_submit = document.getElementById("create_user");
 user_submit.addEventListener("click", createUser, false)
 function createUser() {
-    
+    //stocker l'utilisateur dans une liste de users (creer la liste si elle n'existe pas)
+    //assigner setItem("current_user", JSON.stringify(user))
 }
