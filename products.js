91dfcd1b04b46753016f17b3a28bf727f79a6960
FredericNguyen <fredericnguyen@yahoo.com>
2023-05-08 01:15:22 -0400

    displayed products with flex

--- products.js
+++ products.js
@@ -29,21 +29,63 @@ fetch('http://api.weatherapi.com/v1/current.json?key=' + key + "&q=London", {
     console.error('An error occured. ', error);
 });
 
-function loadProducts() {
-    let pageNum = document.getElementById("currentPage");
-
-}
-
-function buttonHtmlPages(productsLength) {
+function buttonHtmlPages() {
     let productsDOM = document.getElementById("products-container")
-    for (let i=0; i<Math.floor(productsLength/10);i++) {
+    for (let i=0; i<10;i++) {
         let buttonPage = document.createElement("button");
-        buttonPage.innerText = i;
+        buttonPage.innerText = i+1;
         buttonPage.classList.add("buttonPages")
-        buttonPage.addEventListener("click", listProducts)
+        buttonPage.addEventListener("click", listProducts);
+        document.getElementById("pagesButtons").appendChild(buttonPage)
+    }
+    if (!document.getElementById("current-page")) {
+        document.getElementById("pagesButtons").firstChild.click()
+    }
+    function listProducts(event) {
+        event.preventDefault();
+        productsDOM.innerHTML = "";
+        if (document.getElementById("current-page")) {
+            document.getElementById("current-page").id = "";
+        }
+        this.id = "current-page";
+        let pageNumber = Number(this.innerText)-1;
+        
+        fetch('https://dummyjson.com/products?limit=0')
+            .then(res => res.json())
+            .then(data => {
+                const PAGE_MAX = 10;
+                let i;
+                pageNumber == 0 ? i = 0 : i = (pageNumber*PAGE_MAX);
+                let max = i + 10;
+                if (max >= 100) {
+                    max = 100;
+                }
+                localStorage.setItem("products", JSON.stringify(data.products))
+                for (i; i < (max); i++) {
+                    let product = document.createElement("div");
+                    product.classList.add("product");
+                    let productImg = document.createElement("img");
+                    let productNamePrice = document.createElement("div");
+                    productNamePrice.style.textAlign = "center";
+                    let productName = document.createElement("div");
+                    productName.style.fontWeight = "bold";
+                    let productPrice = document.createElement("div");
+                    productPrice.style.color = "grey";
+                    productPrice.style.fontSize = "0.75em";
+                    let productData = data.products[i];
+                    product.id = "product-" + productData.id;
+                    productImg.setAttribute("src", productData.thumbnail);
+                    productName.innerText = productData.title;
+                    productPrice.innerText = "$" + productData.price;
+
+                    productNamePrice.appendChild(productName);
+                    productNamePrice.appendChild(productPrice);
+                    product.appendChild(productImg);
+                    product.appendChild(productNamePrice);
+                    productsDOM.appendChild(product);
+                }
+            });
     }
 }
 
-fetch('https://dummyjson.com/products?limit=0')
-.then(res => res.json())
-.then(data => console.log(data));
+buttonHtmlPages();
