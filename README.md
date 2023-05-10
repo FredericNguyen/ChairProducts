149440651915e52409af55e41dc5b7461a5cf69f
FredericNguyen <fredericnguyen@yahoo.com>
2023-05-10 14:28:32 -0400

    link to help for star rating

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # ChairProducts
+https://stackoverflow.com/questions/71796869/fontawesome-css-stars-half-rating
--- selected_product.js
+++ selected_product.js
@@ -1,21 +1,29 @@
 let currentProduct = JSON.parse(localStorage.getItem("products"))[Number(localStorage.getItem("selectedProduct"))-1];
+
 let linkTitle = document.createElement("div")
 let productsLink = document.createElement("a")
 let title = currentProduct.title
 productsLink.setAttribute("href", "./products.html")
+productsLink.id("productsLink")
 linkTitle.appendChild(productsLink)
-linkTitle.innerText = title
+linkTitle.innerText = " / " + title
+
 let infoDOM = document.getElementById("productInfo")
 let titleDOM = document.createElement("div")
 titleDOM.innerText = title
+
 let categoryDOM = document.createElement("div")
 categoryDOM.innerText = currentProduct.category
+
 let brandDOM = document.createElement("div")
 brandDOM.innerText = currentProduct.brand
-let ragingDOM = document.createElement("div")
-ragingDOM.innerText = currentProduct.raging
+
+let ratingDOM = document.createElement("div")
+ratingDOM.innerText = currentProduct.rating
 let priceDOM = document.createElement("div")
-priceDOM.innerText = currentProduct.price
-let categoryDOM = document.createElement("div")
-categoryDOM.innerText = currentProduct.category
+
+priceDOM.innerText = toString(currentProduct.price) + "  " + toString(currentProduct.price / (1+(12.96/100)))
+
+let descriptionDOM = document.createElement("div")
+descriptionDOM.innerText = currentProduct.description
 
